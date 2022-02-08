857addba5c97d95e2d5ee5aa0ce3a76caef5b08e
Alex <alexo@southern.edu>
2022-02-08 02:19:36 -0500

    Click working and Unit Tested

--- src/click.cpp
+++ src/click.cpp
@@ -1,32 +1,13 @@
-//******************************************************************************
-// Copyright © 2021  Alexander O'Connor
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//******************************************************************************
-
-
 #include "click.h"
 #include <chrono>
 
-namespace PiEar {
-    void mainloop_click(int *cpm, bool *output, bool *end) {
-        while (!*end) {
-            auto start_time = std::chrono::system_clock::now();
-            auto curr_time = std::chrono::system_clock::now();
-            while (std::chrono::duration<double>(curr_time - start_time).count() < (60.0 / (double) *cpm)) {
-                curr_time = std::chrono::system_clock::now();
-                *output = false;
-            }
-            *output = true;
+void mainloop_click(int *cpm, bool *output, bool *end) {
+    while(!*end) {
+        auto start_time = std::chrono::system_clock::now();
+        auto curr_time = std::chrono::system_clock::now();
+        while (std::chrono::duration<double>(curr_time - start_time).count() < (60.0 / (double) *cpm)) {
+            curr_time = std::chrono::system_clock::now();
+            *output = false;
         }
     }
 }
