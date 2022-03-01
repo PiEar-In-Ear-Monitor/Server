0830326c1957e9b254f7e3677e821754e362e56e
Alex <alexo@southern.edu>
2022-03-01 13:57:14 -0500

    Just pushing updates

--- http/controllers/PiEar_bpm.cc
+++ http/controllers/PiEar_bpm.cc
@@ -6,18 +6,18 @@ namespace PiEar {
         //Verify the validity of the token, etc.
         //Read the database or cache to get user information
         Json::Value ret;
-        ret["result"]="ok";
-        ret["bpm"]=100;
-        auto resp=drogon::HttpResponse::newHttpJsonResponse(ret);
+        ret["result"] = "ok";
+        ret["bpm"] = 100;
+        auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
         callback(resp);
     }
-    void bpm::post_bpm(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> &&callback) const {
+    void bpm::post_bpm(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> &&callback, int new_bpm) const {
         //Verify the validity of the token, etc.
         //Read the database or cache to get user information
-        Json::Value ret;
-        ret["result"]="ok";
-        ret["bpm"]=100;
-        auto resp=drogon::HttpResponse::newHttpJsonResponse(ret);
+        LOG_DEBUG << "New BPM: " << new_bpm;
+        Json::Value json_return;
+        json_return["result"] = "ok";
+        auto resp = drogon::HttpResponse::newHttpJsonResponse(json_return);
         callback(resp);
     }
 }
--- http/controllers/PiEar_bpm.h
+++ http/controllers/PiEar_bpm.h
@@ -5,10 +5,10 @@ namespace PiEar {
 class bpm : public drogon::HttpController<bpm> {
     public:
         METHOD_LIST_BEGIN
-        METHOD_ADD(bpm::get_bpm, "/bpm", drogon::Get);
-        METHOD_ADD(bpm::post_bpm, "/bpm", drogon::Post);
+            METHOD_ADD(bpm::get_bpm, "/bpm", drogon::Get);
+            ADD_METHOD_VIA_REGEX(bpm::post_bpm, "/bpm/[0-9]*", drogon::Post);
         METHOD_LIST_END
         void get_bpm(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback) const;
-        void post_bpm(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback) const;
+        void post_bpm(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback, int new_bpm) const;
     };
 }
