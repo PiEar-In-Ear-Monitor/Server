33f1277329d0aabb6168cab6ed51dc11f2ba71f8
Alex <alexo@southern.edu>
2022-04-23 20:09:14 -0400

    Working on it

--- src/audio.cpp
+++ src/audio.cpp
@@ -16,30 +16,28 @@ namespace PiEar {
         }
         else std::cout << "PortAudio initialized" << std::endl;
         // Find out how many devices there are
-        int numDevices = Pa_GetDeviceCount();
-        std::cout << "Number of devices: " << numDevices << std::endl;
-        // Print out the names of the devices
-        for( int i = 0; i < numDevices; i++ ) {
-            const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(i);
-            std::cout << "Device " << i << ": " << deviceInfo->name << std::endl;
-        }
+        const PaDeviceInfo* defaultDeviceInfo = Pa_GetDeviceInfo(Pa_GetDefaultOutputDevice()); // TODO: Make this configurable
+//        int numDevices = Pa_GetDeviceCount();
+//        std::cout << "Number of devices: " << numDevices << std::endl;
+//        // Print out the names of the devices
+//        for( int i = 0; i < numDevices; i++ ) {
+//            const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(i);
+//            std::cout << "Device " << i << ": " << deviceInfo->name << std::endl;
+//            std::cout << "\tMax Input Channels: " << deviceInfo->maxInputChannels << std::endl;
+//            std::cout << "\tMax Output Channels: " << deviceInfo->maxOutputChannels << std::endl;
+//            std::cout << "\tDefault Sample Rate: " << deviceInfo->defaultSampleRate << std::endl;
+//        }
         // Open the default device
         PaStream* stream;
         /* Open an audio I/O stream. */
         err = Pa_OpenDefaultStream( &stream,
-                                    64,          /* input channels */
-                                    0,          /* stereo output */
-                                    paFloat32,  /* 32 bit floating point output */
-                                    44100,
-                                    256,        /* frames per buffer, i.e. the number
-                                                   of sample frames that PortAudio will
-                                                   request from the callback. Many apps
-                                                   may want to use
-                                                   paFramesPerBufferUnspecified, which
-                                                   tells PortAudio to pick the best,
-                                                   possibly changing, buffer size.*/
-                                    paCallback, /* this is your callback function */
-                                    nullptr ); /*This is a pointer that will be passed to your callback*/
+                                    defaultDeviceInfo->maxInputChannels,  /* Max input */
+                                    defaultDeviceInfo->maxInputChannels,  /* Max output */
+                                    paInt16,                              /* 16 Bit Audio */
+                                    defaultDeviceInfo->defaultSampleRate, /* Best sample rate possible from device TODO: Should this always be so high? */
+                                    FRAMES_PER_BUFFER,                    /* frames per buffer TODO: consider: paFramesPerBufferUnspecified */
+                                    paCallback,                           /* this is your callback function */
+                                    nullptr );                            /* This is a pointer that will be passed to your callback */
         if( err != paNoError ) {
             std::cout << "PortAudio error: " << Pa_GetErrorText(err) << std::endl;
             throw std::runtime_error("PortAudio error opening stream");
@@ -67,18 +65,18 @@ namespace PiEar {
         Pa_Terminate();
     }
 
-    int Audio::paCallback( const void *inputBuffer, void *outputBuffer,
-                           unsigned long framesPerBuffer,
-                           const PaStreamCallbackTimeInfo* timeInfo,
-                           PaStreamCallbackFlags statusFlags,
-                           void *userData ) {
+    int Audio::paCallback( const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData ) {
         /* Cast data passed through stream to our structure. */
         paTestData *data = (paTestData*)userData;
         float *out = (float*)outputBuffer;
         (void) inputBuffer; /* Prevent unused variable warning. */
+        std::cout << "Size of input buffer: " << sizeof(inputBuffer) << std::endl;
+        std::cout << "Size of frames per buffer: " << framesPerBuffer << std::endl;
         for(unsigned int i=0; i<framesPerBuffer; i++ )
         {
             // TODO: add audio processing here
+            // Add audio to queue
+//            audioQueue.push(out[i]);
         }
         return 0;
     }
